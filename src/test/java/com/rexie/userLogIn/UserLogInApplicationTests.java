d0b4e5c7a28c142c846608df1e8316244008f925
esmerexie <esmeraldarexie@gmail.com>
2022-09-01 18:17:12 -0700

    initialized app

---
+++ src/test/java/com/rexie/userLogIn/UserLogInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rexie.userLogIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLogInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
